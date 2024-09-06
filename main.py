a651ee395a1ecf93e4e646f8a0b77f4b5479bc5a
Phann020126 <daniel020126@gmail.com>
2024-09-06 17:27:50 -0400

    generativeai api

---
+++ main.py
@@ -0,0 +1,7 @@
+from src.llm.generative_ai import GenAIModel
+
+model = GenAIModel()
+
+ans = model.ask_model("What is the meaning of life?")
+
+print(ans)
---
+++ src/llm/generative_ai.py
@@ -0,0 +1,15 @@
+import google.generativeai as genai
+from dotenv import dotenv_values
+
+secrets = dotenv_values(".env")
+
+GOOGLE_API_KEY = secrets["GOOGLE_API_KEY"]
+
+class GenAIModel():
+    def __init__(self) -> None:
+        genai.configure(api_key=GOOGLE_API_KEY)
+        self.model = genai.GenerativeModel('gemini-1.5-flash')
+        
+    def ask_model(self, question:str) -> str:
+        return self.model.generate_content(question).text
+
