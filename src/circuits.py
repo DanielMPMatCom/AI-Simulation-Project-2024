653f2d682b9995b6bf70a534f8025c3be2ebee7f
Phann020126 <daniel020126@gmail.com>
2024-09-28 19:00:03 -0400

    mock electric consume

--- src/circuits.py
+++ src/circuits.py
@@ -1,20 +1,39 @@
 from src.people import Citizen
 from utils.gaussianmixture import *
-
+import random
 
 class Circuit:
     """
     Represents an electricity circuit consisting of multiple blocks.
     """
 
-    def __init__(self, id) -> None:
+    def __init__(self, id, gaussian_mixture, blocks_range = (4, 8), citizens_range = (100, 200)) -> None:
         self.id = id
-        self.mock_electric_consume = 200 # Modify this value is important
-        self.blocks: list[Block] = []
+        self.blocks_range = blocks_range
+        self.citizens_range = citizens_range
+        self.gaussian_mixture = gaussian_mixture
+        self.blocks: list[Block] = self.create_bolcks()
+        self.mock_electric_consume = self.generate_mock_consume()
 
     def update(self):
         for block in self.blocks:
             block.update()
+
+    def create_bolcks(self):
+        blocks = []
+        amount_of_blocks = random.randint(self.blocks_range[0], self.blocks_range[1])
+        for i in range(amount_of_blocks):
+            blocks.append(Block(self.gaussian_mixture, self.citizens_range))
+        
+        return blocks
+    
+    def generate_mock_consume(self):
+        mock_value = 0
+        for block in self.blocks:
+            mock_value += block.mock_electric_consume
+
+        return mock_value
+
 
 
 class Block:
@@ -22,12 +41,13 @@ class Block:
     Represents a block within a circuit that consumes energy.
     """
 
-    def __init__(self, gaussianmixture : DailyElectricityConsumptionBimodal) -> None:
+    def __init__(self, gaussian_mixture : DailyElectricityConsumptionBimodal, citizens_range) -> None:
         self.citizens: list[Citizen] = []
         self.history_report: list[str] = []
         self.off_hours: tuple[int, int] = (0, 0)
         self.demand_per_hour: list[float] = []
-        self.gaussianmixture = gaussianmixture
+        self.gaussian_mixture = gaussian_mixture
+        self.mock_electric_consume = self.get_mock_electric_consume()
 
     def update(self):
         pass
@@ -40,3 +60,9 @@ class Block:
         return sum(self.demand_per_hour[: self.off_hours[0]]) + sum(
             self.demand_per_hour[self.off_hours[1] :]
         )
+    
+    def get_mock_electric_consume(self) -> float:
+        mock_value = 0
+        for i in range(300):
+            day_consume = sum(self.gaussian_mixture.generate())
+            mock_value = max(mock_value, day_consume)
