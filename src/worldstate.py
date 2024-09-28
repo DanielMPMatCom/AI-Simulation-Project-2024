75b334979a19fe04cfbb5ec3ce1b599df4a04805
Phann020126 <daniel020126@gmail.com>
2024-09-28 02:09:21 -0400

    world state initialization

--- src/worldstate.py
+++ src/worldstate.py
@@ -6,5 +6,15 @@ from map import *
 
 
 class WorldState:
-    def __init__(self, energy_demand) -> None:
-        self.energy_demand = energy_demand
+    def __init__(self, circuits:list[Circuit], thermoelectrics:list[Thermoelectric]) -> None:
+        
+        self.circuits = circuits
+        self.thermoelectrics = thermoelectrics
+
+        self.update()
+
+    def update(self):
+
+        self.total_offer = sum([thermoelectric.current_capacity for thermoelectric in self.thermoelectrics])
+        self.total_demand = sum([(block.get_consumed_energy_today for block in circuit.blocks) for circuit in self.circuits])
+        self.total_deficit = min(self.total_demand - self.total_offer)
